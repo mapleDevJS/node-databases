3288b6ff670f4465104927f3d8d3639c3bf1f440
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-05-15 19:51:08 -0400

    query data from MongoDB

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -41,7 +41,9 @@ class MongoBackend {
     return this.collection.insertMany(documents);
   }
 
-  async getMax() {}
+  async getMax() {
+    return this.collection.findOne({}, { sort: { value: 1 } });
+  }
 
   async max() {
     console.log("Connection to MongoDB");
@@ -61,10 +63,20 @@ class MongoBackend {
 
     console.info(`Inserted ${insertResult.result.n} documents into MongoDB`);
 
+    console.info("Querying MongoDB");
+    console.time("mongodb-find");
+    const doc = await this.getMax();
+    console.timeEnd("mongodb-find");
+
     console.log("Disconnecting from MongoDB");
     console.time("mongodb-disconnect");
     await this.disconnect();
     console.timeEnd("mongodb-disconnect");
+
+    return {
+      date: doc.date,
+      value: doc.value
+    };
   }
 }
 
